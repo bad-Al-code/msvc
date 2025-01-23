1d09458a3acd086e85cdcdf598a4f693f92721d2
badAl <badalsingh120403@gmail.com>
2025-01-23 16:50:55 +0530

    feat(posts): list all posts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import PostCreate from "./components/PostCreate";
+import PostList from "./components/PostList";
 
 function App() {
   return (
-    <div className="bg-black h-screen flex items-center justify-center">
+    <div className="bg-black h-screen flex flex-col gap-10 items-center justify-center">
       <PostCreate />
+      <PostList />
     </div>
   );
 }
